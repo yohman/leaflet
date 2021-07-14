c463463616955b9b9c80ad12433a51eb48ac988f
yohman <yohman@gmail.com>
2021-07-14 09:55:31 -0700

    My first map

---
+++ js/map.js
@@ -0,0 +1,9 @@
+var map = L.map('map').setView([34.0697,-118.4432], 17);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var marker = L.marker([34.0697,-118.4432]).addTo(map)
+		.bindPopup('The Technology Sandbox<br> Where Yoh is sitting this very moment')
+		.openPopup();	
